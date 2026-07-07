f612550da0351c8342b11344fc9f7910f7a391e1
arihant-dev <jainarj411@gmail.com>
2026-07-07 16:18:59 +0200

    Update unit tests to match new StartDownload and StartUpload signatures

--- src/backend/go/pkg/transfer/transfer_test.go
+++ src/backend/go/pkg/transfer/transfer_test.go
@@ -41,7 +41,7 @@ func TestFileTransferSessionCreation(t *testing.T) {
 		}
 	}()
 
-	err = ft.StartDownload("trans_123", "docs/file.txt", "peer_bob", "hash123", 100, "127.0.0.1", netPort, 420)
+	err = ft.StartDownload("trans_123", "docs/file.txt", "repo_123", "peer_bob", "hash123", 100, "127.0.0.1", netPort, 420)
 	if err != nil {
 		t.Fatalf("start download failed: %v", err)
 	}
@@ -119,7 +119,7 @@ func TestFileTransferStreaming(t *testing.T) {
 		_, _ = io.WriteString(conn, mockData)
 	}()
 
-	err = ft.StartDownload("dl_session_1", "notes.txt", "peer_alice", "hashxyz", int64(len(mockData)), "127.0.0.1", p2pPort, 420)
+	err = ft.StartDownload("dl_session_1", "notes.txt", "repo_123", "peer_alice", "hashxyz", int64(len(mockData)), "127.0.0.1", p2pPort, 420)
 	if err != nil {
 		t.Fatalf("start download: %v", err)
 	}
@@ -191,7 +191,7 @@ func TestIPv6AddressHandling(t *testing.T) {
 		}
 	}()
 
-	err = ft.StartDownload("trans_ipv6", "docs/file.txt", "peer_bob", "hash123", 100, "::1", netPort, 420)
+	err = ft.StartDownload("trans_ipv6", "docs/file.txt", "repo_123", "peer_bob", "hash123", 100, "::1", netPort, 420)
 	if err != nil {
 		t.Fatalf("StartDownload failed with IPv6: %v", err)
 	}
@@ -236,7 +236,7 @@ func TestFileTransferSizeMismatch(t *testing.T) {
 	}()
 
 	// We expect 10 bytes, but mockData is only 5 bytes.
-	err = ft.StartDownload("session_mismatch", "notes.txt", "peer_alice", "hashxyz", 10, "127.0.0.1", p2pPort, 420)
+	err = ft.StartDownload("session_mismatch", "notes.txt", "repo_123", "peer_alice", "hashxyz", 10, "127.0.0.1", p2pPort, 420)
 	if err != nil {
 		t.Fatalf("start download: %v", err)
 	}
@@ -296,7 +296,7 @@ func TestFileTransferUploadStreaming(t *testing.T) {
 	ft := NewFileTransferManager(ipcServer)
 
 	mockData := "Upload file content"
-	netPort, err := ft.StartUpload("up_session_1", "docs/file.txt", "peer_bob", "hash123", int64(len(mockData)))
+	netPort, err := ft.StartUpload("up_session_1", "docs/file.txt", "repo_123", "peer_bob", "hash123", int64(len(mockData)))
 	if err != nil {
 		t.Fatalf("StartUpload failed: %v", err)
 	}
@@ -374,7 +374,7 @@ func TestFileTransferUploadSizeMismatch(t *testing.T) {
 	ft := NewFileTransferManager(ipcServer)
 
 	// Expect 20 bytes, but C++ will write only 5 bytes
-	netPort, err := ft.StartUpload("up_session_mismatch", "docs/file.txt", "peer_bob", "hash123", 20)
+	netPort, err := ft.StartUpload("up_session_mismatch", "docs/file.txt", "repo_123", "peer_bob", "hash123", 20)
 	if err != nil {
 		t.Fatalf("StartUpload failed: %v", err)
 	}
